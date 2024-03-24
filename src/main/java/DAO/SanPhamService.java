6e8bf7793969437fcad577fd8567eea2ecf6fbeb
PresidentHuTao <pmhoang29084@gmail.com>
2024-03-24 12:05:24 +0700

    khởi tạo

---
+++ src/main/java/DAO/SanPhamService.java
@@ -0,0 +1,13 @@
+package DAO;
+
+import Model.SanPham;
+
+import java.util.ArrayList;
+
+public class SanPhamService {
+    private ArrayList<SanPham> sp = new ArrayList<>();
+
+    private void addSP(SanPham sp) {
+        sp = new SanPham();
+    }
+}
---
+++ src/main/java/Model/SanPham.java
@@ -0,0 +1,21 @@
+package Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SanPham {
+    private String id;
+    private String maSanPham;
+    private String tenSanPham;
+    private float gia;
+    private String mauSac;
+    private String kichThuoc;
+    private int soLuong;
+
+}
